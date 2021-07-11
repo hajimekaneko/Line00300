4964b7dcebc26a2f46dd31e54e79b6df1ab576da
hajimekaneko <hajime.10600@gmail.com>
2021-07-11 16:55:07 +0900

    test

--- scrape.py
+++ scrape.py
@@ -3,34 +3,59 @@ import urllib.request
 import json
 import requests
 
-url = 'https://news.yahoo.co.jp/topics'
+url = 'https://news.google.com/search'
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) '\
     'AppleWebKit/537.36 (KHTML, like Gecko) '\
     'Chrome/67.0.3396.99 Safari/537.36 '
+news_text = []
+news_link = []
 
 def getNews(word):
-   html = requests.get(url)
-   soup = bs4(html.content, "html.parser")
+    params = {'hl':'ja', 'gl':'JP', 'ceid':'JP:ja', 'q':word}
+    # url、パラメータを設定してリクエストを送る
+    res = requests.get(url, params=params)
+    # レスポンスをBeautifulSoupで解析する
+    soup = bs4(res.content, "html.parser")
+    # レスポンスからh3階層のニュースを抽出する（classにxrnccdを含むタグ）
+    h3_blocks = soup.select(".xrnccd")
+    for i, h3_entry in enumerate(h3_blocks):
+        # 記事を10件だけ処理する
+        if len(news_text) == 3:
+            break
+        # ニュースのタイトルを抽出する（h3タグ配下のaタグの内容）
+        h3_title = h3_entry.select_one("h3 a").text
+        # ニュースのリンクを抽出する（h3タグ配下のaタグのhref属性）
+        h3_link = h3_entry.select_one("h3 a")["href"]
+        # 抽出したURLを整形して絶対パスを作る
+        h3_link = urllib.parse.urljoin(url, h3_link)
+        news_text.append(h3_title)
+        news_link.append(h3_link)
 
-   main = soup.find_all('li',attrs={'class': 'topicsListItem'})
 
-   news_text = []
-   news_link = []
+        # h3階層のニュースからh4階層のニュースを抽出する
+        h4_block = h3_entry.select_one(".SbNwzf")
 
-   for i in range(0,len(main)):
-       news_text.append(main[i].get_text())
-       news_link.append(main[i].a.get("href"))
+        if h4_block != None:
+            # h4階層が存在するときのみニュースを抽出する
+            h4_articles = h4_block.select("article")
 
-   count = 0
-   news = []
+            for j, h4_entry in enumerate(h4_articles):
+                h4_title = h4_entry.select_one("h4 a").text
+                h4_link = h4_entry.select_one("h4 a")["href"]
+                h4_link = urllib.parse.urljoin(url, h4_link)
+                news_text.append(h4_title)
+                news_link.append(h4_link)
 
-   for i in range(0,len(news_text)):
-           if news_text[i].find(word) > -1:
-               news.append(news_text[i])
-               news.append(news_link[i])
-               count += 1
-   if count == 0:
-       news.append("記事が見つかりませんでした！！")
+    count = 0
+    news = []
 
-   result = '\n'.join(news)
-   return result
+    for i in range(0,len(news_text)):
+            if news_text[i].find(word) > -1:
+                news.append(news_text[i])
+                news.append(news_link[i])
+                count += 1
+    if count == 0:
+        news.append("記事が見つかりませんでした！！")
+
+    result = '\n'.join(news)
+    return result
---
+++ test.py
@@ -0,0 +1,6 @@
+import scrape as sc
+
+
+word = "日向坂"
+result = sc.getNews(word)
+print(result)
